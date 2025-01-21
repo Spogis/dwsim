9611afc23a5fc8c5a98b811ba856e5a926c7399d
Daniel Wagner Oliveira de Medeiros <danielwag@gmail.com>
2025-01-21 09:43:31 -0300

    update

--- DWSIM.AI.ConvergenceHelper/Classes/Manager.cs
+++ DWSIM.AI.ConvergenceHelper/Classes/Manager.cs
@@ -63,6 +63,35 @@ namespace DWSIM.AI.ConvergenceHelper
             {
                 lock (Database)
                 {
+                    var comps = data.CompoundNames.OrderBy(x => x).ToList();
+                    var comps0 = data.CompoundNames.ToList();
+                    var mf1 = new List<string>();
+                    var mf2 = new List<string>();
+                    var vf = new List<string>();
+                    var lf1 = new List<string>();
+                    var lf2 = new List<string>();
+                    var sf = new List<string>();
+                    var k1 = new List<string>();
+                    var k2 = new List<string>();
+                    foreach (var comp in comps) {
+                        mf1.Add(data.MixtureMolarFlows[comps0.IndexOf(comp)]);
+                        if (data.MixtureMolarFlows2 != null) if (data.MixtureMolarFlows2 != null) mf2.Add(data.MixtureMolarFlows2[comps0.IndexOf(comp)]);
+                        if (data.VaporMolarFlows != null) vf.Add(data.VaporMolarFlows[comps0.IndexOf(comp)]);
+                        if (data.Liquid1MolarFlows != null) lf1.Add(data.Liquid1MolarFlows[comps0.IndexOf(comp)]);
+                        if (data.Liquid2MolarFlows != null) lf2.Add(data.Liquid2MolarFlows[comps0.IndexOf(comp)]);
+                        if (data.SolidMolarFlows != null) sf.Add(data.SolidMolarFlows[comps0.IndexOf(comp)]);
+                        if (data.KValuesVL1 != null) k1.Add(data.KValuesVL1[comps0.IndexOf(comp)]);
+                        if (data.KValuesVL2 != null) k2.Add(data.KValuesVL2[comps0.IndexOf(comp)]);
+                    }
+                    data.CompoundNames = comps.ToArray();
+                    data.MixtureMolarFlows = mf1.ToArray();
+                    if (data.MixtureMolarFlows2 != null) data.MixtureMolarFlows2 = mf2.ToArray();
+                    if (data.VaporMolarFlows != null) data.VaporMolarFlows = vf.ToArray();
+                    if (data.Liquid1MolarFlows != null) data.Liquid1MolarFlows = lf1.ToArray();
+                    if (data.Liquid2MolarFlows != null) data.Liquid2MolarFlows = lf2.ToArray();
+                    if (data.SolidMolarFlows != null) data.SolidMolarFlows = sf.ToArray();
+                    if (data.KValuesVL1 != null) data.KValuesVL1 = k1.ToArray();
+                    if (data.KValuesVL2 != null) data.KValuesVL2 = k2.ToArray();
                     data.Hash = data.GetBase64StringHash();
                     var col = Database.GetDatabaseObject().GetCollection<ConvergenceHelperTrainingData>("TrainingData");
                     var entries = col.Query().Where(x => x.Hash == data.Hash);
